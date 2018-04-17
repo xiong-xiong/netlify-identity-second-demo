0718c410cea6f9036fc4bf05ed158765bdb038c7
Juha Stenroos <juha.stenroos@edita.fi>
2018-04-17 14:54:24 +0300

    hello world

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Hello world
         </p>
         <LambdaDemo/>
       </div>
